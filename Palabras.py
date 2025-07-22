bc5090aee722c02555732d5750e4dc54f9d9c27d
Ricardo <148652964+ricr78@users.noreply.github.com>
2025-07-22 16:54:15 -0600

    Update Palabras.py

--- Palabras.py
+++ Palabras.py
@@ -1,7 +1,7 @@
 def palindromo(palabra: str) -> bool:
     palabra = palabra.lower().replace(" "," ")
     return palabra == palabra[::-1]
-
+# entrada de la palabra que me permite interactuar si es o no con una función que lleva a describir el fragmento de codigo simple
 out = input("Escribe una palabra: ")
 if palindromo(out):
     print("es palindromo.")
